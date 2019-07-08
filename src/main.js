a9d0e87ff3b0207a17f8b0d72512e6d9f3ae70d2
gyyxtx <1042766239@qq.com>
2019-07-08 21:58:29 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@
 // ES6中的模块化的语法,导入模块
 import Vue from 'vue'
 import App from './App.vue'
+
+// 导入vue-router
+import VueRouter from 'vue-router'
+// 注册VueRouter插件
+Vue.use(VueRouter)
+// 创建路由对象
+const router = new VueRouter({
+  // 配置路由规则
+  route:[]
+})
+
 // 导入css样式
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
@@ -12,4 +23,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 配置router
+  router
 }).$mount('#app')
