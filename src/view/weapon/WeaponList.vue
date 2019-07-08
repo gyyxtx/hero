b6199897e7f06aea9e6b377dbf2cd6f1e17a3e73
gyyxtx <1042766239@qq.com>
2019-07-09 00:12:23 +0800

    路由组件 实现武器列表和装备列表

---
+++ src/view/weapon/WeaponList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <h2 class="sub-header">武器列表</h2>
+    <a class="btn btn-success" href="add.html">Add</a>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1,001</td>
+            <td>Lorem</td>
+            <td>ipsum</td>
+            <td>dolor</td>
+            <td>sit</td>
+            <td>
+              <a href="edit.html">edit</a>
+              &nbsp;&nbsp;
+              <a href="javascript:window.confirm('Are you sure?')">delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
