e6b14c9af765875223fb1ecf1510f49bf0287782
gyyxtx <1042766239@qq.com>
2019-07-09 08:54:22 +0800

    实现删除功能

--- src/view/hero/HeroList.vue
+++ src/view/hero/HeroList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
     <h2 class="sub-header">英雄列表</h2>
-    <a class="btn btn-success" href="add.html">Add</a>
+    <a class="btn btn-success" href="add.html">添加</a>
     <div class="table-responsive">
       <table class="table table-striped">
         <thead>
@@ -20,7 +20,7 @@
             <td>
               <a href="edit.html">编辑</a>
               &nbsp;&nbsp;
-              <a href="javascript:window.confirm('Are you sure?')">删除</a>
+              <a href="javascript:" @click="del(item.id)">删除</a>
             </td>
           </tr>
         </tbody>
@@ -42,17 +42,51 @@ export default {
     this.showList()    
   },
   methods:{
+    //获取英雄列表数据并将其渲染到页面上
     showList:function(){
       axios
         .get('http://localhost:3000/heroes')
         .then(res=>{
-          console.log(res)
+          // console.log(res)
+          // 解构赋值
           const {status,data}=res;
+          // 判断是否成功
           if(status==200){
+            // 将后端获取的data数据复制给data中的list
             this.list=data;
+          }else{
+            alert('获取列表失败')
           }
         })
-    }
+        .catch((err)=>{
+          alert('服务器异常'+err)
+        })
+    },
+    // 删除英雄列表
+    del(id){
+      // 接收传的参数id,拼接在请求地址
+      // 判断是否要删除,如果不删,则跳出程序
+      if(!confirm('你确定要删除吗?')){
+        return false;
+      }
+      // 如果确定,发送请求,删除对应行
+      axios
+      .delete(`http://localhost:3000/heroes/${id}`)
+      .then(res=>{
+        // console.log(res)
+        // 如果删除成功,更新列表
+        if(res.status==200){
+          // 重新调用函数,重新渲染页面,实现页面更新
+          this.showList()
+        }else{
+          alert('删除英雄失败')
+        }
+      })
+      .catch((err)=>{
+        alert('服务器异常')
+      })
+    },
+    // 添加功能
   }
 };
 </script>
